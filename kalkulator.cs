51801450b4ca0a1ba7fd6b272d5320b636b82a0c
rafafawwaz <rafafawwaz2@students.amikom.ac.id>
2024-03-05 10:25:39 +0700

    rafa added Main function

--- kalkulator.cs
+++ kalkulator.cs
@@ -2,7 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+			int a = 10;
+			int b = 6;
 			
+			Console.WriteLine("Hasil Penjumlahan: {0} + {1} = {2}", a, b, Penjumlahan(a, b));
+			Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+			
+			Console.WriteLine("\nPress Twice to Exit");
+			Console.ReadKey();	
 	}
 	
 	static int Penjumlahan(int a, int b)
